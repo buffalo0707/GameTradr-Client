ac1459ec636be839042119e3fd040a12312c4328
buffalo0707 <alan.henderson@libertymutual.com>
2017-06-04 14:38:04 -0400

    Imports required modules in AuthService
    AuthService was causing server to crash due to missing components. Fixed
    by adding Observable and HTTP modules.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class AuthService {
+  private apiUrl = 'http://localhost:4741';
+  constructor(private http: Http) { }
 
-  constructor() { }
-
+  post(body: any): Observable<Response> {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      const updateUrl = `${this.apiUrl}/sign-in`;
+      return this.http.post(updateUrl, JSON.stringify(body), { headers })
+      .map((res:Response) => res.json());
+    }
 }
