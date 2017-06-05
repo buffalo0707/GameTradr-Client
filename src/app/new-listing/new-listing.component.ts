50909fe979f4394810910af01e6ec240163bc8ea
buffalo0707 <alan.henderson@libertymutual.com>
2017-06-05 15:59:35 -0400

    Updates new-listing component form controls

--- src/app/new-listing/new-listing.component.ts
+++ src/app/new-listing/new-listing.component.ts
@@ -13,10 +13,14 @@ export class NewListingComponent implements OnInit {
     game:  {},
     wanted: {}
   }
+  searchName: any
+  searchWantedName: any
   systems: any = []
   games: any = []
-  loading = false
+  wantedGames: any = []
+  loading = true
   disableGame = true
+  disableWantedGame = true
   constructor(
     private listingService: ListingService,
     private gameService: GameService,
@@ -26,6 +30,7 @@ export class NewListingComponent implements OnInit {
       this.gameService.onSystemsRetrieved((data: any) =>{
         this.systems = data.Data.Platforms.Platform
         console.log(this.systems)
+        this.loading = false
       })
     }
 
@@ -48,14 +53,38 @@ export class NewListingComponent implements OnInit {
     this.disableGame = false
   }
 
+  enableWantedGame(){
+    this.disableWantedGame = false
+  }
+
   gameSearch(){
     let query = {}
     query['system']= this.listing.game.system
-    query['name']=this.listing.game.name
+    query['name']=this.searchName
     console.log('query is', query)
     this.gameService.onGamesRetrieved(query, (data: any) =>{
       this.games = data.Data.Game
     })
+  }
+
+  wantedGameSearch(){
+    let query = {}
+    query['system']= this.listing.wanted.system
+    query['name']=this.searchWantedName
+    console.log('query is', query)
+    this.gameService.onGamesRetrieved(query, (data: any) =>{
+      this.wantedGames = data.Data.Game
+      console.log(data)
+    })
+  }
+
+  onGameSelectionChange(game){
+    this.listing.game.name = game.GameTitle
+    console.log(game)
+  }
+
+  onWantedGameSelectionChange(game){
+    this.listing.wanted.name = game.GameTitle
   }
 
 }
